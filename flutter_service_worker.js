1c60221f89f3feda8a06c09c9538d1d159d1537a
Husein <76621738+dev-husein@users.noreply.github.com>
2022-09-19 00:00:39 +0300

    added item view search

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -88,9 +88,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1f24dd8f99251c35c5f00be78b8ef228",
-"/": "1f24dd8f99251c35c5f00be78b8ef228",
-"main.dart.js": "0a0cc3ecb1839f5f7ea7859780624d48",
+"index.html": "188b7ac35da14114215b5f4b6ff8b7fc",
+"/": "188b7ac35da14114215b5f4b6ff8b7fc",
+"main.dart.js": "ba5a1a55b90bf423fdd21617b021365d",
 "manifest.json": "01503b56540502436110b869fd681f72",
 "version.json": "b1288b4733c3813ac778fa831e6666d9"
 };
