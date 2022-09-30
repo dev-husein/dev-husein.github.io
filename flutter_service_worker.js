d7cfa8a81319beb6adbac396a2b2069654c6f3de
Husein <76621738+dev-husein@users.noreply.github.com>
2022-10-01 00:08:13 +0300

    drug companies added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -89,9 +89,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fa3a2a770234b69882905d957c960ebf",
-"/": "fa3a2a770234b69882905d957c960ebf",
-"main.dart.js": "c08dc75421f898de7320a03741e1e7b1",
+"index.html": "890ef16df37ea2f00d45bde10dffcf0e",
+"/": "890ef16df37ea2f00d45bde10dffcf0e",
+"main.dart.js": "a4f43c83ea5d8c8a8ae10429b06c1c8b",
 "manifest.json": "01503b56540502436110b869fd681f72",
 "version.json": "b1288b4733c3813ac778fa831e6666d9"
 };
