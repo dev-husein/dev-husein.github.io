89cf343417e874e32d045092f51f81d079055049
Husein <76621738+dev-husein@users.noreply.github.com>
2024-08-20 02:48:44 +0300

    5.9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2568486435"
+    serviceWorkerVersion: "559321897"
   }
 });
