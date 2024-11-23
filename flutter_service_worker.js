9139c3b26caeb703b4c59aa66585f67ec4fe6e90
husein <dev.husein@gmail.com>
2024-11-24 02:07:35 +0300

    updated ui 6.3.5.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fc1183208f61e9f92f0b608c7a4092a5",
+const RESOURCES = {"flutter_bootstrap.js": "132203ae16c868ced29ef18b0abfd35d",
 "version.json": "3590d229f23ee364b85edd2cdd43f749",
 "index.html": "c81c58d384c868e1548a4bcb9d5bfc13",
 "/": "c81c58d384c868e1548a4bcb9d5bfc13",
-"main.dart.js": "bca5e5cf992ad84824a1c5254da8c9a5",
+"main.dart.js": "d2ddb72b69183708e521fa621929faf9",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
