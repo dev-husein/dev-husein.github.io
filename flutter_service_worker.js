3f641632d40afa2159a862152e4d0741183bc97a
husein <dev.husein@gmail.com>
2024-12-09 01:12:40 +0300

    6.3.8.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f94758499e36d4958e826a51eb3f0e70",
+const RESOURCES = {"flutter_bootstrap.js": "81356b8580567cd8c87d03c4098d2d2d",
 "version.json": "3590d229f23ee364b85edd2cdd43f749",
 "index.html": "c81c58d384c868e1548a4bcb9d5bfc13",
 "/": "c81c58d384c868e1548a4bcb9d5bfc13",
-"main.dart.js": "cfb484217bc0c97da564507c320aeddb",
+"main.dart.js": "ae318ac013c1337aa2e0a02974bc66fe",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
