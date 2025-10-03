9e320bf03576d0d634595960fb49ee3e1f683499
husein <dev.husein@gmail.com>
2025-10-03 22:31:32 +0300

    6.8.6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1941516b080aad50388a98a1f19592be",
-"version.json": "c98daa0ad2c8609501ec2c45e745bc0c",
+const RESOURCES = {"flutter_bootstrap.js": "74241d65882e33dba0c517670c85552f",
+"version.json": "45f7816373fbcd35d39e7081802dc86b",
 "index.html": "87acc6372324a79170098240599ec46f",
 "/": "87acc6372324a79170098240599ec46f",
-"main.dart.js": "bc80c0f8abbad3fef39ee8b65d4a2243",
+"main.dart.js": "9cf10eedeac959897d305a4b3600ca78",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -17,7 +17,7 @@ const RESOURCES = {"flutter_bootstrap.js": "1941516b080aad50388a98a1f19592be",
 "manifest.json": "bef1c8000e4050228d096e99f8557c70",
 "assets/shorebird.yaml": "8a6cc29d444eb0f524471e8cbbe90089",
 "assets/AssetManifest.json": "e7ffbd2c979d8a8428e7887098620ff3",
-"assets/NOTICES": "482f78c9b598fc2937cc47cef2e4f877",
+"assets/NOTICES": "47c294905dad58aa272c57180c5c2069",
 "assets/image/img.png": "5ac8b53bd9b5f042119d061659515d22",
 "assets/image/new.png": "2cf36bbe15d549d64b3092f6b2ad25bf",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
