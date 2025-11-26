347a1524b161c5a00a81b45c33e24e3fa7f59d90
husein <dev.husein@gmail.com>
2025-11-27 01:57:13 +0300

    7.0.4 update 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fe2f271a70a3be240b463f18936507eb",
+const RESOURCES = {"flutter_bootstrap.js": "403e5728f600fb7558dd276f9e4465bd",
 "version.json": "14d688c93084e811d2e3548aae0fd41e",
 "index.html": "87acc6372324a79170098240599ec46f",
 "/": "87acc6372324a79170098240599ec46f",
-"main.dart.js": "1ad49edde73d2dfb531be13a3bdacee8",
+"main.dart.js": "46ce32a9a8320e55c3ecb8bb8932e165",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
